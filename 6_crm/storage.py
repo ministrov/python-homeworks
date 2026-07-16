58295b0c868bd3fc9521b4b1ba1c334482bad3e4
Zhila <ministrov2018@gmail.com>
2026-07-16 11:14:15 +0300

    feat(6_crm): implement storage.load

--- 6_crm/storage.py
+++ 6_crm/storage.py
@@ -1,11 +1,25 @@
 """ Модуль хранилища JSON данных"""
 
+import json
+from pathlib import Path
 from orders import Order
 
 
-def load(path: str):
+def load(path: str) -> list[Order]:
     """ Загрузить список заказов из JSON файла """
-    pass
+    if not Path(path).exists():
+        return []
+    with open(path, "r", encoding="utf-8") as f:
+        try:
+            raw = json.load(f)
+            print(raw)
+        except json.JSONDecodeError as e:
+            print(f"Файл поврежден {e}")
+            return []
+    for order in raw:
+        order["tags"] = set(order["tags"])
+
+    return raw
 
 
 def save(orders: list[Order], path: str):
