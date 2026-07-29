5d3276f4663afd63b33b85f35ff2115f9ea0280c
Zhila <ministrov2018@gmail.com>
2026-07-29 13:53:52 +0300

    feat(3_oop_base): add LuxuryRoom subclass

--- intermediate/3_oop_base/3_oop_base.py
+++ intermediate/3_oop_base/3_oop_base.py
@@ -33,3 +33,27 @@ class Room:
             f"Номер комнаты: № {self.room_number} "
             f"Цена за ночь: {self.price_per_night}"
         )
+
+
+class LuxuryRoom(Room):
+    def __init__(self, room_number: str | int, price_per_night: int | float, price_multiplier: float):
+        super().__init__(room_number, price_per_night)
+        self.price_multiplier = price_multiplier
+
+    def get_price(self) -> int | float:
+        return self.price_per_night * self.price_multiplier
+
+    def show_info(self) -> str:
+        base = super().show_info()
+        return (
+            f"{base} "
+            f"Увеличение цены на: {self.price_multiplier}"
+        )
+
+
+if __name__ == "__main__":
+    room = Room("12", 12.40)
+    luxury_room = LuxuryRoom("23", 23, 1.5)
+
+    print(room.show_info())
+    print(luxury_room.show_info())
