278835cb7844d9adbfe5d8bc65dc108e48b124d2
Zhila <ministrov2018@gmail.com>
2026-08-14 13:13:26 +0300

    feat(5_slots): explore User internals via ast/dict introspection
    - Dump AST of the source file
    - Inspect anton.__dict__ and User.__dict__
    - Leave commented dis/getrefcount snippets for later experiments

--- intermediate/5_slots/user_and_slots.py
+++ intermediate/5_slots/user_and_slots.py
@@ -7,7 +7,18 @@
 """
 
 
+import ast
+# import dis
+# import sys
+
+
+source = open(__file__).read()
+print(ast.dump(ast.parse(source), indent=2))
+
+
 class User:
+    """ Обычный пользователь """
+
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
@@ -15,3 +26,8 @@ class User:
 
 anton = User("Anton", 34)
 print(anton.name)
+
+# dis.dis(User.__init__)
+# print(sys.getrefcount(anton))
+print(anton.__dict__)
+print(User.__dict__)
